5aa5c5f37c4863c13b1fca68691810e59c0ae308
Huy <qnhuy213@gmail.com>
2024-10-05 18:48:58 +0700

    Project installation for backend

---
+++ Backend/src/test/java/com/REACT_API/API_backend/ApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.REACT_API.API_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
